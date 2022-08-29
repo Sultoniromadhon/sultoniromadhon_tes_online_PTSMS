6179eaeb5433d7a7fe7208fdf4d7ff3af7edb67b
Sultoniromadhon <zagzegzagzegzagzeg@gmail.com>
2022-08-29 12:03:33 +0700

    Jam ke dua

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [UserController::class, 'login']);
+Route::post('tambahuser', [UserController::class, 'tambahuser']);
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'update']);
+    Route::post('logout', [UserController::class, 'logout']);
 });
